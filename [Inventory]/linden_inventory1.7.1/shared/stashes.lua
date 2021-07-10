0a3513461a4da3a0857d5bfb0c598d2a7563ba98
aidanohart <44532573+aidanohartt@users.noreply.github.com>
2021-07-10 11:37:22 -0700

    Removed older inventories

--- [Inventory]/linden_inventory1.7.1/shared/stashes.lua
+++
@@ -1,8 +0,0 @@
-Config.PoliceEvidence = vector3(474.2242, -990.7516, 26.2638) -- /evidence # while near this point
-
-Config.Stashes = {
-	--{ coords = vector3(474.2242, -990.7516, 26.2638), slots = 70, name = 'Police Evidence', job = 'police' }, --using command instead
-	{ coords = vector3(-1194.712, -891.7582, 13.98022), slots = 70, name = 'BurgerShot', owner = true, job = 'police' },
-
-	{ coords = vector3(301.3054, -600.2374, 43.2821), slots = 70, name = 'Personal Locker', owner = true, job = 'ambulance'  },
-}
