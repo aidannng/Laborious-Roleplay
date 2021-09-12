35e536f2d933cb38505ee1bf5df0eacd1e6b51bd
aidanohart <44532573+aidanohartt@users.noreply.github.com>
2021-09-12 16:27:56 -0700

    v9.9.5.2

---
+++ [Businesses]/labrp_zerohes/server.lua
@@ -0,0 +1,67 @@
+ESX = nil
+
+TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+
+--------------------------------------------------------------------------------------------------------------------------------------------
+
+-- Server Callbacks
+
+RegisterServerEvent('craft:duster')
+AddEventHandler('craft:duster', function()
+    local xPlayer = ESX.GetPlayerFromId(source)
+    local scrapQuantity = xPlayer.getInventoryItem('scrapmetal').count
+    local steelQuantity = xPlayer.getInventoryItem('steel').count
+
+    if scrapQuantity >= 5  then
+        if steelQuantity >= 10 then
+            xPlayer.addInventoryItem('WEAPON_KNUCKLE', 1)
+            xPlayer.removeInventoryItem('scrapmetal', 5)
+            xPlayer.removeInventoryItem('steel', 10)
+        else
+            TriggerClientEvent('mythic_notify:client:SendAlert', source, { type = 'error', text = 'Insufficient Materials!', })
+        end
+    end
+end)
+
+RegisterServerEvent('craft:machete')
+AddEventHandler('craft:machete', function()
+    local xPlayer = ESX.GetPlayerFromId(source)
+    local scrapQuantity = xPlayer.getInventoryItem('scrapmetal').count
+    local steelQuantity = xPlayer.getInventoryItem('steel').count
+    local rubberQuantity = xPlayer.getInventoryItem('rubber').count
+
+    if scrapQuantity >= 5 then
+        if steelQuantity >= 10 then
+            if rubberQuantity >= 10 then
+                xPlayer.addInventoryItem('WEAPON_MACHETE', 1)
+                xPlayer.removeInventoryItem('scrapmetal', 5)
+                xPlayer.removeInventoryItem('rubber', 3)
+                xPlayer.removeInventoryItem('steel', 10)
+        
+            else
+                TriggerClientEvent('mythic_notify:client:SendAlert', source, { type = 'error', text = 'Insufficient Materials!', })
+            end
+        end
+    end  
+end)
+
+RegisterServerEvent('craft:blade')
+AddEventHandler('craft:blade', function()
+    local xPlayer = ESX.GetPlayerFromId(source)
+    local scrapQuantity = xPlayer.getInventoryItem('scrapmetal').count
+    local steelQuantity = xPlayer.getInventoryItem('steel').count
+    local rubberQuantity = xPlayer.getInventoryItem('rubber').count
+
+    if scrapQuantity >= 5 then
+        if steelQuantity >= 10 then
+            if rubberQuantity >= 10 then
+                xPlayer.addInventoryItem('WEAPON_SWITCHBLADE', 1)
+                xPlayer.removeInventoryItem('scrapmetal', 5)
+                xPlayer.removeInventoryItem('rubber', 2)
+                xPlayer.removeInventoryItem('steel', 5)
+            else
+                TriggerClientEvent('mythic_notify:client:SendAlert', source, { type = 'error', text = 'Insufficient Materials', })
+            end
+        end
+    end  
+end)
